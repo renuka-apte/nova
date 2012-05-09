c6210a06acf75ea9b0ae4d6c3241e12a33633012
Renuka Apte <renuka.apte@citrix.com>
2012-05-09 14:18:55 -0700

    Boot from volume, first pass
    Change-Id: If117087086eab809217d2b173f921bf9319a52c7

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -416,7 +416,10 @@ class ComputeManager(manager.SchedulerDependentManager):
                         {'connection_info': jsonutils.dumps(cinfo)})
                 block_device_mapping.append({'connection_info': cinfo,
                                              'mount_device':
-                                             bdm['device_name']})
+                                             bdm['device_name'],
+                                             'delete_on_termination':
+                                             bdm['delete_on_termination']})
+                LOG.debug(_("%s"),block_device_mapping)
 
         return {
             'root_device_name': instance['root_device_name'],
@@ -670,7 +673,9 @@ class ComputeManager(manager.SchedulerDependentManager):
             cinfo = jsonutils.loads(bdm['connection_info'])
             block_device_mapping.append({'connection_info': cinfo,
                                          'mount_device':
-                                         bdm['device_name']})
+                                         bdm['device_name'],
+                                         'delete_on_termination':
+                                         bdm['delete_on_termination']})
         # NOTE(vish): The mapping is passed in so the driver can disconnect
         #             from remote volumes if necessary
         return {'block_device_mapping': block_device_mapping}
